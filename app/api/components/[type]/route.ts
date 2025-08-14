964268f1960ef9a248fc380043d776c993741111
Szymon <rogalaszymon78@gmail.com>
2025-08-14 15:24:04 +0200

    fix bug 3

--- app/api/components/[type]/route.ts
+++ app/api/components/[type]/route.ts
@@ -1,22 +1,17 @@
 import prisma from "@/prisma/client";
 import {NextResponse} from "next/server";
-export async function GET(req: Request, context: { params: Record<string, string> }){
+
+interface Params {
+    params: { type: string };
+}
+
+export async function GET(req: Request, context: Params){
     try {
         const type = context.params.type;
 
         const components = await prisma.component.findMany({
             where: {
                 type
-            },
-            select: {
-                id: true,
-                type: true,
-                name: true,
-                price: true,
-                imageUrl: true,
-                description: true,
-                specs: true,
-                 // <- upewnij się że to jest tutaj
             }});
         if(components.length === 0){
             return NextResponse.json({success: false,error:null, message:'Brak komponentów o podanym typie' },{status:404})
